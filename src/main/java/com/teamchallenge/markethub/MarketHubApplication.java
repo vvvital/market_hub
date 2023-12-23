bd8411a40fb96f30866b25cdb67f502bbc4c0f80
kamui <evgennkuvs@gmail.com>
2023-12-24 01:48:57 +0200

    first commit

---
+++ src/main/java/com/teamchallenge/markethub/MarketHubApplication.java
@@ -0,0 +1,13 @@
+package com.teamchallenge.markethub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamchallenge/markethub/MarketHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamchallenge.markethub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
