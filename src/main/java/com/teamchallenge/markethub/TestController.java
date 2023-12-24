6df95364fd6098da7cb7db320a071077fb006848
kamui <evgennkuvs@gmail.com>
2023-12-24 02:08:51 +0200

    added TestController

---
+++ src/main/java/com/teamchallenge/markethub/TestController.java
@@ -0,0 +1,16 @@
+package com.teamchallenge.markethub;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @GetMapping()
+    public String test() {
+        return "Hello World";
+    }
+}
